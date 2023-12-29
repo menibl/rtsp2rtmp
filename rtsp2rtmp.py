6cfdeaef493fb675584a75c9506af38633ab3616
menibl <62341961+menibl@users.noreply.github.com>
2023-12-29 22:05:11 +0200

    Update rtsp2rtmp.py

--- rtsp2rtmp.py
+++ rtsp2rtmp.py
@@ -1,18 +1,28 @@
+from flask import Flask, request, render_template_string
 import subprocess
 import os
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        rtsp_url = request.form['rtsp_url']
+        rtmp_url = request.form['rtmp_url']
+        run_ffmpeg(rtsp_url, rtmp_url)
+        return 'FFmpeg process started.'
+    return '''
+        <form method="post">
+            RTSP URL: <input type="text" name="rtsp_url"><br>
+            RTMP URL: <input type="text" name="rtmp_url"><br>
+            <input type="submit" value="Run FFmpeg">
+        </form>
+    '''
+
 def run_ffmpeg(rtsp_url, rtmp_url):
     ffmpeg_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ffmpeg.exe")
-       
-   ## ffmpeg_command = r"d:\ffmpeg-2023-12-04-git-8c117b75af-essentials_build\bin\ffmpeg.exe -rtsp_transport tcp -i {} -c:a aac -ar 44100 -ac 1 -c copy -f flv {}"
-
     ffmpeg_command = f'"{ffmpeg_path}" -rtsp_transport tcp -i {rtsp_url} -c:a aac -ar 44100 -ac 1 -c copy -f flv {rtmp_url}'
     subprocess.run(ffmpeg_command, shell=True)
 
-def main():
-    rtsp_url = input("Enter RTSP URL: ")
-    rtmp_url = input("Enter RTMP URL: ")
-    run_ffmpeg(rtsp_url, rtmp_url)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
